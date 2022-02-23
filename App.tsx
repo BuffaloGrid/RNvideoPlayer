2bbc82cbe79cc72d5c686bda5c394b070ed645d0
Andres <andres.carrera@buffalogrid.com>
2022-02-23 09:26:25 +0000

    added bitmovin library

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import Video from "react-native-video";
+import React, { useRef } from 'react';
+import {StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import ReactNativeBitmovinPlayer, {
+  ReactNativeBitmovinPlayerMethodsType,
+} from '@takeoffmedia/react-native-bitmovin-player';
 //import manifest from "./assets/124366fd-93e3-435d-9c0b-c77e87b159bf/manifest.mpd"
 
 //const manifest = require("./assets/124366fd-93e3-435d-9c0b-c77e87b159bf/cmaf/video-H264-360-800k-video-avc1.mp4")
 
 const App = () => {
+  const ref = useRef<ReactNativeBitmovinPlayerMethodsType>();
   return (
-    <View style={styles.viewHeight}>
-       <Video
-        source={{ uri:"file:///RNvideoPlayer/assets/124366fd-93e3-435d-9c0b-c77e87b159bf/cmaf/manifest.mpd", type:"mpd"}}
-        //source={{uri:"assets/124366fd-93e3-435d-9c0b-c77e87b159bf/cmaf/manifest.mpd"}}
-        resizeMode={"cover"}
-        style={styles.backgroundVideo}
-        allowsExternalPlayback={true}
-        fullscreenAutorotate={true}
-        controls={true}
-        />
-    </View>
+    <SafeAreaView style={styles.viewHeight}>
+    <ReactNativeBitmovinPlayer
+      autoPlay={false}
+      hasZoom={false}
+      style={styles.backgroundVideo}
+      configuration={{
+        startOffset: 0,
+        hasNextEpisode: false,
+        url: 'https://ftp.itec.aau.at/datasets/DASHDataset2014/BigBuckBunny/15sec/BigBuckBunny_15s_simple_2014_05_09.mpd',
+      }}
+    />
+    </SafeAreaView>
   );
 };
 
@@ -29,17 +29,20 @@ const styles = StyleSheet.create({
   backgroundVideo: {
     bottom: 0,
     left: 0,
-    position: "absolute",
+    position: 'absolute',
     right: 0,
     top: 0,
-
-},
-viewHeight: {
-  alignSelf:"center",
-  justifyContent:"center",
-  height: 300,
-  width: "100%",
-},
+  },
+  viewHeight: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: '100%',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
 });
 
 export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# RNvideoPlayer
+
+This repository is for testing `react-native-video` with exoplayer as a video player in a empty project to evaluate whether react native video works with ofline playbacks and more esential features.
